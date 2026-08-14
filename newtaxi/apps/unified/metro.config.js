9d811bbc294e695e350134832170fef547464038
Naveenagowda2005 <naveenagowda09@gmail.com>
2026-08-15 00:31:15 +0530

    Fix: Update metro.config.js for Node.js compatibility on EAS Build servers

--- newtaxi/apps/unified/metro.config.js
+++ newtaxi/apps/unified/metro.config.js
@@ -2,4 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for Node.js compatibility - avoid using toReversed() on EAS Build servers
+// by explicitly setting up the config without relying on newer array methods
+config.resolver = {
+  ...config.resolver,
+  sourceExts: process.env.RN_SRC_EXT
+    ? process.env.RN_SRC_EXT.split(',').concat(config.resolver.sourceExts || [])
+    : config.resolver.sourceExts,
+};
+
 module.exports = config;
